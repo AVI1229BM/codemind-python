87913db9dea0e0b9b5998ba746649a492d7bb543
AVI1229BM <115801045+AVI1229BM@users.noreply.github.com>
2022-10-23 13:36:18 +0530

    n = int(input()) for i in range(1,n+1):   for j in range(1,n+1):     if i == j:         print('0',end="")     else:         print('x',end="")   print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,6 +1,8 @@
 n = int(input())
-asci = 64
-for i in range(n,0,-1):
-    for j in range(i):
-        print(chr(asci+i),end=" ")
-    print()
+for i in range(1,n+1):
+  for j in range(1,n+1):
+    if i == j:
+        print('0',end="")
+    else:
+        print('x',end="")
+  print()
