600086d201b445be50c6e0098a605bc43b0288f3
AVI1229BM <115801045+AVI1229BM@users.noreply.github.com>
2022-10-23 19:13:22 +0530

    n = int(input()) def fab(n):     a = 0     b = 1     print(a,end=" ")     print(b,end=" ")     for i in range(2,n):         c = a+b         a=b         b=c         print(c,end=" ") if n < 1000:     fab(n)

---
+++ Play_with_Fibonacci_sequence.py
@@ -0,0 +1,13 @@
+n = int(input())
+def fab(n):
+    a = 0
+    b = 1
+    print(a,end=" ")
+    print(b,end=" ")
+    for i in range(2,n):
+        c = a+b
+        a=b
+        b=c
+        print(c,end=" ")
+if n < 1000:
+    fab(n)
