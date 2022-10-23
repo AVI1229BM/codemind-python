7b107a56202fb081500c8e57f1445b836c13c268
AVI1229BM <115801045+AVI1229BM@users.noreply.github.com>
2022-10-23 13:45:26 +0530

    n = int(input()) ch = 'A' for i in range(n):   for j in range(n):     print(ch,end=" ")   print()   ch = chr(ord(ch)+1)

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,6 +1,7 @@
 n = int(input())
+ch = 'A'
 for i in range(n):
-    for j in range(n,0,-1):
-        print(j,end=" ")
-    print()
-    
+  for j in range(n):
+    print(ch,end=" ")
+  print()
+  ch = chr(ord(ch)+1)
