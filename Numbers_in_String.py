564492324f6556011cc7b565dc42fc4f984b3ba9
AVI1229BM <115801045+AVI1229BM@users.noreply.github.com>
2022-10-25 18:21:19 +0530

    n  = input() sum = 0 for i in  n:     if i ==  '1' or i == '2' or i == '3' or i == '4' or i == '5' or i == '6' or i == '7' or i == '8' or i == '9':        sum += int(i) print(sum)

---
+++ Numbers_in_String.py
@@ -0,0 +1,6 @@
+n  = input()
+sum = 0 
+for i in  n:
+    if i ==  '1' or i == '2' or i == '3' or i == '4' or i == '5' or i == '6' or i == '7' or i == '8' or i == '9':
+       sum += int(i)
+print(sum)
