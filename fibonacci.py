73ab8599de31953b031554d33eeaba2821359e80
AVI1229BM <115801045+AVI1229BM@users.noreply.github.com>
2022-10-25 20:24:52 +0530

    n = int(input()) def fab(n):     a=0     b=1     print(a,end=' ')     print(b,end=' ')     for i in range(2,n):         c=a+b         a=b         b=c         print(c,end=' ') fab(n)

--- fibonacci.py
+++ fibonacci.py
@@ -1,13 +1,12 @@
 n = int(input())
-
-def fab():
-    a = 0
-    b = 1
-    print(a,end=" ")
-    print(b,end=" ")
+def fab(n):
+    a=0 
+    b=1
+    print(a,end=' ')
+    print(b,end=' ')
     for i in range(2,n):
-       c = a+b
-       a = b
-       b = c
-       print(c,end=" ")
-fab()
+        c=a+b
+        a=b
+        b=c
+        print(c,end=' ')
+fab(n)
