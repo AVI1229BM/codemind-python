5f5c2a6b00c9f0a374bf1462d36544618283242b
AVI1229BM <115801045+AVI1229BM@users.noreply.github.com>
2022-10-25 17:45:33 +0530

    n  = int(input()) for i in range(n):     j = input()     if '1' in j or '2' in j or '3' in j or '4' in j or '5' in j or '6' in j or '7' in j or '8' in j or '9' in j:        print("Yes")     else:         print("No")

---
+++ string_contains_digits_or_not.py
@@ -0,0 +1,7 @@
+n  = int(input())
+for i in range(n):
+    j = input()
+    if '1' in j or '2' in j or '3' in j or '4' in j or '5' in j or '6' in j or '7' in j or '8' in j or '9' in j:
+       print("Yes")
+    else:
+        print("No")
